20d5f9f7cb14a99459e296f7a3f47282aa05f4ad
Angelisium <angelisium@daum.net>
2023-08-31 18:44:40 +0200

    Update thread.ts

--- src/app/pages/thread/thread.ts
+++ src/app/pages/thread/thread.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user';
 
 @Component({
 	selector: 'app-thread',
 	templateUrl: './template.html',
 })
 export class ThreadPage {
-	constructor() {}
+	constructor(
+		// service
+		public service: UserService
+	) {}
 }
