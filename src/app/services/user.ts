4bad4c87783559b8ba6f7751ceb8d8ade4573e19
Angelisium <angelisium@daum.net>
2023-08-31 11:49:35 +0200

    Create user.ts

---
+++ src/app/services/user.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+export interface User {
+	id: string;
+	name: string;
+}
+
+@Injectable({
+	providedIn: 'root',
+})
+export class UserService {
+	public user?: User;
+	private users: User[] = [
+		{ id: '6d6ba1eb-3f9f-41b3-88af-fd3a959faeaa', name: 'Angelisium' },
+		{ id: 'e6d8b859-22ea-4d4d-a958-f7677a23a38b', name: 'Alice' },
+	];
+
+	constructor() {
+		const username = localStorage.getItem('username');
+		if (username) {
+			this.login(username);
+		}
+	}
+
+	login(name: string) {
+		const user = this.users.find((u) => u.name == name);
+		if (user) {
+			localStorage.setItem('username', user.name);
+			this.user = user;
+			return true;
+		}
+		return false;
+	}
+}
