4e92c4929d549e753fe116288789c2ed091f8ba1
Angelisium <angelisium@daum.net>
2023-08-31 15:41:20 +0200

    Update thread.ts

---
+++ src/app/services/thread.ts
@@ -0,0 +1,43 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Thread {
+	id: string;
+	label: string;
+	locked: boolean;
+}
+
+@Injectable({
+	providedIn: 'root',
+})
+export class ThreadService {
+	thread?: Thread;
+	threads: Thread[] = [];
+	onChange: EventEmitter<Thread> = new EventEmitter();
+
+	constructor(
+		// Services
+		private http: HttpClient
+	) {
+		this.fetch();
+	}
+
+	fetch() {
+		const url = `/api/v1/threads`;
+		const request = this.http.get<Thread[]>(url);
+		request.subscribe((threads: Thread[]) => {
+			this.threads = threads;
+		});
+		// TODO : gestion erreurs
+	}
+
+	change(id: string): boolean {
+		const thread = this.threads.find((t) => t.id == id);
+		if (thread) {
+			this.onChange.emit(thread);
+			this.thread = thread;
+			return true;
+		}
+		return false;
+	}
+}
