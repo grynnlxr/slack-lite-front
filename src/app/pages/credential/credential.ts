0c3d94f058fa1e0e7b852d57d7a73ba8be4be805
Angelisium <angelisium@daum.net>
2023-08-31 11:49:15 +0200

    Update credential.ts

--- src/app/pages/credential/credential.ts
+++ src/app/pages/credential/credential.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User, UserService } from 'src/app/services/user';
 
 @Component({
 	selector: 'app-credential',
 	templateUrl: './template.html',
 })
-export class CredentialPage {}
+export class CredentialPage {
+	name: string = '';
+
+	constructor(private service: UserService) {}
+
+	onSubmit(form: NgForm) {
+		const name = form.value.name;
+		if (!this.service.login(name)) {
+			// TODO : afficher une popup "utilisateur inconnu"
+			console.log('bad username');
+		}
+	}
+}
