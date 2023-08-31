12c4b8326a5c7047eb1fdebfa64b61e27682f81b
Angelisium <angelisium@daum.net>
2023-08-31 11:49:22 +0200

    Update main.ts

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { User, UserService } from 'src/app/services/user';
 
 @Component({
 	selector: 'app-main',
 	templateUrl: './template.html',
 })
-export class MainPage {}
+export class MainPage {
+	constructor(
+		// Services
+		public userService: UserService
+	) {}
+}
