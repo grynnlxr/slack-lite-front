52e09b56cb1c5465a33daa548ca5c73c7f21fdb2
Angelisium <angelisium@daum.net>
2023-09-01 18:57:06 +0200

    Update item.ts

--- src/app/components/message/item.ts
+++ src/app/components/message/item.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Message, MessageService } from 'src/app/services/message';
+import { UserService } from 'src/app/services/user';
 
 @Component({
 	selector: 'message-item',
@@ -10,8 +11,16 @@ export class MessageItemComponent {
 
 	constructor(
 		// service
-		private service: MessageService
+		private service: MessageService,
+		private userService: UserService
 	) {}
+
+	isTheAuthor(): boolean {
+		if (this.data.author.id == this.userService.user?.id) {
+			return true;
+		}
+		return false;
+	}
 
 	remove() {
 		this.service.delete(this.data.id);
