06b72e675e15652b4c0c007f2c39e1b46c33fd1a
Angelisium <angelisium@daum.net>
2023-09-01 16:12:39 +0200

    Update list.ts

--- src/app/components/thread/list.ts
+++ src/app/components/thread/list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Thread, ThreadService } from 'src/app/services/thread';
 
 @Component({
@@ -6,12 +7,24 @@ import { Thread, ThreadService } from 'src/app/services/thread';
 	templateUrl: './list.template.html',
 })
 export class ThreadListComponent {
+	label!: string;
+
 	constructor(
 		// services
 		public service: ThreadService
 	) {}
 
 	changeTo(thread: Thread) {
+		if (thread.editMode) {
+			return;
+		}
 		this.service.change(thread.id);
+	}
+
+	onSubmit(form: NgForm) {
+		const label = form.value.label;
+		// TODO : empecher les ajout de thread sans nom
+		this.service.create(label);
+		form.reset();
 	}
 }
