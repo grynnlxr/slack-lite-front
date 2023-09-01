33994af32221c8faf89a13ab69f43067aacd8566
Angelisium <angelisium@daum.net>
2023-09-01 14:23:37 +0200

    Update list.ts

--- src/app/components/message/list.ts
+++ src/app/components/message/list.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MessageService } from 'src/app/services/message';
+import { ThreadService } from 'src/app/services/thread';
 
 @Component({
 	selector: 'message-list',
 	templateUrl: './list.template.html',
 })
 export class MessageListComponent {
+	message!: string;
+
 	constructor(
 		// services
-		public service: MessageService
+		public messageService: MessageService,
+		public threadService: ThreadService
 	) {}
+
+	// 0: pas de thread choisi, 1: loading, 2: pas de message, 3: des messages
+	getState(): number {
+		if (!this.threadService.thread) {
+			return 0;
+		}
+		if (this.messageService.loading) {
+			return 1;
+		}
+		if (this.messageService.messages.length < 1) {
+			return 2;
+		}
+		return 3;
+	}
+
+	addMessage(form: NgForm) {
+		const content = form.value.message;
+		this.messageService.create(content);
+		form.reset();
+	}
 }
